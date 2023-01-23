9c2c4dd5e15ee7b35462b0102966e4300a18058b
Nana Reinikainen <nana.reinikainen@helsinki.fi>
2023-01-23 13:08:18 +0200

    kurssitiedot step 4

--- osa1/kurssitiedot/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/kurssitiedot/src/App.js
@@ -1,23 +1,25 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content part={[part1, part2, part3]} />
-      <Total exercises={[part1.exercises, part2.exercises, part3.exercises]} />
+      <Content part={parts} />
+      <Total exercises={parts} />
     </div>
   )
 }
@@ -41,7 +43,7 @@ const Content = (props) => {
 const Total = (props) => {
   return(
     <p>
-      Number of exercises {props.exercises[0] + props.exercises[1] + props.exercises[2]}
+      Number of exercises {props.exercises[0].exercises + props.exercises[1].exercises + props.exercises[2].exercises}
     </p>
   )
 }
