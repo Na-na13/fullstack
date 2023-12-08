55de72991922cea72a79b4034615a88d216be6f7
Nana Reinikainen <nana.reinikainen@helsinki.fi>
2023-12-08 21:53:23 +0200

    blogilistan frontend, step3

--- osa5/bloglist-frontend/src/App.jsx
+++ osa5/bloglist-frontend/src/App.jsx
@@ -9,11 +9,15 @@ const App = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
 
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )  
-  }, [])
+  }, [blogs])
 
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem('loggedBloglistUser')
@@ -35,11 +39,12 @@ const App = () => {
       window.localStorage.setItem(
         'loggedBloglistUser', JSON.stringify(user)
       )
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
     } catch (exeption){
-      console.log('error occured', exeption)
+      console.log('error occurred', exeption)
     }
   }
 
@@ -49,13 +54,27 @@ const App = () => {
     window.localStorage.removeItem('loggedBloglistUser')
   }
 
+  const handleCreateBlog = async (event) => {
+    event.preventDefault()
+    try {
+      await blogService.create({
+        title, author, url
+      })
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+    } catch (exeption) {
+      console.log('error occurred', exeption)
+    }
+  }
+
   if (user === null) {
     return (
       <div>
         <h2>Log in to application</h2>
         <form onSubmit={handleLogin}>
           <div>
-            username 
+            username
               <input
               type='text'
               value={username}
@@ -64,7 +83,7 @@ const App = () => {
               />
           </div>
           <div>
-            password 
+            password
             <input
             type='password'
             value={password}
@@ -82,20 +101,44 @@ const App = () => {
     <div>
       <h2>blogs</h2>
       <p>{user.username} is logged in <button onClick={handleLogout}>logout</button></p>
+      <h2>create new</h2>
+      <div>
+        <form onSubmit={handleCreateBlog}>
+            <div>
+              title
+                <input
+                type='text'
+                value={title}
+                name='Title'
+                onChange={({ target }) => setTitle(target.value)}
+                />
+            </div>
+            <div>
+              author
+              <input
+              type='text'
+              value={author}
+              name='Author'
+              onChange={({ target }) => setAuthor(target.value)}
+              />
+            </div>
+            <div>
+              url
+              <input
+              type='text'
+              value={url}
+              name='URL'
+              onChange={({ target }) => setUrl(target.value)}
+              />
+            </div>
+            <button type='submit'>create</button>
+          </form>
+        </div>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
     </div>
   )
-
-  /*return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
-  )*/
 }
 
 export default App
