c679aeb1f81da49263fb2f7bf4176e986fe7bad2
Nana Reinikainen <nana.reinikainen@helsinki.fi>
2023-12-11 20:42:59 +0200

    blogilistan frontend, step7 step8

--- osa5/bloglist-frontend/src/components/Blog.jsx
+++ osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,34 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [visibility, setVisibility] = useState(false)
+  const buttonText = visibility ? "hide" : "view"
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  if (!visibility) {
+    return(
+      <div style={blogStyle}>
+        {blog.title} <button onClick={() => setVisibility(!visibility)}>{buttonText}</button>
+      </div>
+    )
+  } else {
+    return(
+      <div style={blogStyle}>
+        {blog.title} <button onClick={() => setVisibility(!visibility)}>{buttonText}</button> <br/>
+        {blog.author}<br/>
+        {blog.url}<br/>
+        {blog.likes} <button>like</button><br/>
+        {blog.user ? `Added by ${blog.user.username}` : ""}
+      </div>
+    )
+  }
+}
 
 export default Blog
