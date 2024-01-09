8d76d199885be21dce6510fa2ed5b0b45c61b6d6
Nana Reinikainen <nana.reinikainen@helsinki.fi>
2024-01-09 14:31:38 +0200

    tehtävä 6.22

--- osa6/query-anecdotes/src/App.jsx
+++ osa6/query-anecdotes/src/App.jsx
@@ -1,13 +1,23 @@
-import { useQuery } from '@tanstack/react-query'
-import { getAnecdotes } from './requests'
+import { useQuery, useQueryClient, useMutation } from '@tanstack/react-query'
+import { getAnecdotes, voteAnecdote } from './requests'
 
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
 const App = () => {
 
+  const queryClient = useQueryClient()
+
+  const voteAnecdoteMutation = useMutation({
+    mutationFn: voteAnecdote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+    }
+  })
+
   const handleVote = (anecdote) => {
     console.log('vote')
+    voteAnecdoteMutation.mutate({ ...anecdote })
   }
 
   const result = useQuery({
--- osa6/query-anecdotes/src/requests.js
+++ osa6/query-anecdotes/src/requests.js
@@ -7,3 +7,6 @@ export const getAnecdotes = () =>
 
 export const createAnecdote = newAnecdote =>
 	axios.post(baseUrl, newAnecdote).then(response => response.data)
+
+export const voteAnecdote = anecdote =>
+	axios.put(`${baseUrl}/${anecdote.id}`, {...anecdote, votes: anecdote.votes+1})
