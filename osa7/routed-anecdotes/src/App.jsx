e5bdd6f2b23c660d0ad67cb6066d9cd597969b1a
Nana Reinikainen <nana.reinikainen@helsinki.fi>
2024-01-10 14:59:40 +0200

    anekdoottisovellus ja hookit, step2

--- osa7/routed-anecdotes/src/App.jsx
+++ osa7/routed-anecdotes/src/App.jsx
@@ -75,6 +75,12 @@ const CreateNew = (props) => {
     })
   }
 
+  const handleReset = () => {
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -91,7 +97,8 @@ const CreateNew = (props) => {
           url for more info
           <input {...info} />
         </div>
-        <button>create</button>
+        <button type='submit'>create</button>
+        <button type='reset' onClick={handleReset}>reset</button>
       </form>
     </div>
   )
